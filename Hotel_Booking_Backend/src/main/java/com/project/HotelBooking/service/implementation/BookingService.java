5c0c3228d7028e64e6007746db98fd0d3338881c
Bhanu Prakash <bhanuprakash7854@gmail.com>
2025-05-26 11:39:33 +0530

    corrected save booking logic

--- Hotel_Booking_Backend/src/main/java/com/project/HotelBooking/service/implementation/BookingService.java
+++ Hotel_Booking_Backend/src/main/java/com/project/HotelBooking/service/implementation/BookingService.java
@@ -16,10 +16,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
 @Service
-public class BookingService implements BookingServiceInterface
-{
+public class BookingService implements BookingServiceInterface {
 
     @Autowired
     private RoomRepo roomRepo;
@@ -33,18 +31,15 @@ public class BookingService implements BookingServiceInterface
     @Override
     public Response saveBooking(Long roomId, Long userId, Booking booking) {
         Response response = new Response();
-        try
-        {
-            if(booking.getCheckOutDate().isBefore(booking.getCheckInDate()))
-            {
+        try {
+            if (booking.getCheckOutDate().isBefore(booking.getCheckInDate())) {
                 throw new IllegalArgumentException("Check in date must come after check out date");
             }
-            Room room = roomRepo.findById(roomId).orElseThrow(()->new OurException("Room not found"));
-            User user = userRepo.findById(userId).orElseThrow(()->new OurException("User not found"));
+            Room room = roomRepo.findById(roomId).orElseThrow(() -> new OurException("Room not found"));
+            User user = userRepo.findById(userId).orElseThrow(() -> new OurException("User not found"));
 
             List<Booking> bookingList = room.getBookingList();
-            if(!isRoomAvailable(bookingList,booking))
-            {
+            if (!isRoomAvailable(bookingList, booking)) {
                 throw new OurException("Room not available for the selected date range");
             }
             String confirmationCode = Utils.generateRandomString(10);
@@ -55,86 +50,74 @@ public class BookingService implements BookingServiceInterface
             response.setStatusCode(200);
             response.setMessage("Booking sucessful");
             response.setBookingConfirmationCode(confirmationCode);
-        }catch (OurException e)
-        {
+        } catch (OurException e) {
             response.setMessage(e.getMessage());
             response.setStatusCode(404);
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             response.setStatusCode(500);
-            response.setMessage("Error saving booking "+e.getMessage());
+            response.setMessage("Error saving booking " + e.getMessage());
         }
 
         return response;
     }
 
     @Override
-    public Response findBookingByConfirmationCode(String confirmationCode)
-    {
+    public Response findBookingByConfirmationCode(String confirmationCode) {
         Response response = new Response();
-        try
-        {
-            Booking booking = bookingRepo.findByBookingConfirmationCode(confirmationCode).orElseThrow(()->new OurException("Booking not found"));
+        try {
+            Booking booking = bookingRepo.findByBookingConfirmationCode(confirmationCode)
+                    .orElseThrow(() -> new OurException("Booking not found"));
             BookingDto bookingDto = Utils.mapBookingEntityToBookingDto(booking);
             response.setBooking(bookingDto);
             response.setStatusCode(200);
             response.setMessage("Sucessful");
-        }catch (OurException e)
-        {
+        } catch (OurException e) {
             response.setStatusCode(404);
             response.setMessage(e.getMessage());
-        }catch (Exception e)
-        {
-            response.setMessage("Error finding Booking "+e.getMessage());
+        } catch (Exception e) {
+            response.setMessage("Error finding Booking " + e.getMessage());
             response.setStatusCode(500);
         }
         return response;
     }
 
     @Override
-    public Response findAllBookings()
-    {
+    public Response findAllBookings() {
         Response response = new Response();
-        try
-        {
+        try {
             List<Booking> bookingList = bookingRepo.findAll();
             List<BookingDto> bookingDtoList = Utils.mapBookingListToBookingDtoList(bookingList);
             response.setStatusCode(200);
             response.setMessage("Sucessful");
             response.setBookingList(bookingDtoList);
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             response.setStatusCode(500);
-            response.setMessage("Error finding bookings "+e.getMessage());
+            response.setMessage("Error finding bookings " + e.getMessage());
         }
         return response;
     }
 
     @Override
-    public Response cancelBooking(Long bookingId)
-    {
+    public Response cancelBooking(Long bookingId) {
         Response response = new Response();
-        try
-        {
-            Booking booking = bookingRepo.findById(bookingId).orElseThrow(()->new OurException("Booking not found"));
+        try {
+            Booking booking = bookingRepo.findById(bookingId).orElseThrow(() -> new OurException("Booking not found"));
             bookingRepo.deleteById(bookingId);
             response.setMessage("Sucessful");
             response.setStatusCode(200);
-        }catch (OurException e)
-        {
+        } catch (OurException e) {
             response.setStatusCode(404);
             response.setMessage(e.getMessage());
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             response.setStatusCode(500);
-            response.setMessage("Error canceling booking "+e.getMessage());
+            response.setMessage("Error canceling booking " + e.getMessage());
         }
         return response;
     }
 
-    private boolean isRoomAvailable(List<Booking> bookingList, Booking booking)
-    {
-        return bookingList.stream().noneMatch(existingBooking -> !(booking.getCheckOutDate().isBefore(existingBooking.getCheckInDate()) ||
-                booking.getCheckInDate().isAfter(booking.getCheckOutDate())));
+    private boolean isRoomAvailable(List<Booking> bookingList, Booking booking) {
+        return bookingList.stream().noneMatch(
+                existingBooking -> ((booking.getCheckInDate().isBefore(existingBooking.getCheckOutDate()) &&
+                        booking.getCheckOutDate().isAfter(existingBooking.getCheckInDate()))));
     }
 }
